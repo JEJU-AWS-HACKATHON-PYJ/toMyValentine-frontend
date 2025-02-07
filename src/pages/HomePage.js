05163d56239293bdb20522e558968ff9af460012
suwith <suwith@kookmin.ac.kr>
2025-02-07 14:48:31 +0900

    Completed project setting

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,10 +2,7 @@ import React from "react";
 
 const HomePage = () => {
     return (
-      <div className="flex flex-col items-center text-center bg-[#fffad0] h-screen w-full">
     <div className="text-xl ">홈 페이지</div>
-          {/* 컨텐츠 추가 */}
-      </div>
     );
 };
 
